5d3e94c9ff77a0a7968726eb2e4ca4fbf4f57936
Ishan Gaur <ishangaur107@gmail.com>
2023-08-19 07:03:01 +0530

    Remove dependency

--- src/Components/Checkout/Subtotal.jsx
+++ src/Components/Checkout/Subtotal.jsx
@@ -1,30 +1,11 @@
 import React from 'react'
-import CurrencyFormat from 'react-currency-format';
+
 import "./Subtotal.css";
 function Subtotal() {
   return (
     <div className='substotal'>
-      <CurrencyFormat
-      renderText={(value)=>(
-        <>
-        <p>
-            {/* Part of homework */}
-            Subtotal (0 items):
-            <strong>0</strong>
-        </p>
-        <small className='subtotal_gift'>
-            <input type="checkbox" />This order contains a gift
-        </small>
-        </>
-      )}
-      decimalScale={2}
-      value={0} // Part of the homework
-      displayType={"text"}
-      thousandSeparator={true}
-      prefix={"$"}
-      >
-
-      </CurrencyFormat>
+      
+    
         <button>Proceed to checkout</button>
     </div>
   )
